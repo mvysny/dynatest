e6cc4d43f4c6b84b52dc2cad6610c5ec7f734417
Martin Vysny <vysny@baka.sk>
2018-02-19 22:37:39 +0200

    a

--- src/test/kotlin/com/github/mvysny/dynatest/SanityTest.kt
+++ src/test/kotlin/com/github/mvysny/dynatest/SanityTest.kt
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import kotlin.test.expect
 
+/**
+ * If one of these tests fail, there is a really glaring bug in the DynaTest framework. Other test suites will then produce completely
+ * incorrect results and should be ignored.
+ */
 class SanityTest {
     @Test
     fun simple() {
