8b09d1eae239bd6dff06309b1919ee2db1eb4e59
Martin Vysny <martin@vysny.me>
2018-10-16 13:41:10 +0300

    add tests for toTestSource()

--- dynatest/src/test/kotlin/com/github/mvysny/dynatest/TestUtilsTest.kt
+++ dynatest/src/test/kotlin/com/github/mvysny/dynatest/TestUtilsTest.kt
@@ -1,5 +1,8 @@
 package com.github.mvysny.dynatest
 
+import com.github.mvysny.dynatest.engine.DynaNodeGroupImpl
+import com.github.mvysny.dynatest.engine.toTestSource
+import org.junit.platform.engine.support.descriptor.FileSource
 import java.io.IOException
 import java.util.*
 import java.util.concurrent.ConcurrentHashMap
@@ -119,6 +122,15 @@ class TestUtilsTest : DynaTest({
             expect("a") { "a".cloneBySerialization() }
             expect("") { "".cloneBySerialization() }
             expect(25) { 25.cloneBySerialization() }
+        }
+    }
+
+    group("tests for StackTraceElement.toTestSource()") {
+        test("this class resolves to FileSource") {
+            val e = DynaNodeGroupImpl.computeTestSource()!!
+            val src = e.toTestSource() as FileSource
+            expect(true, src.file.absolutePath) { src.file.absolutePath.endsWith("src/test/kotlin/com/github/mvysny/dynatest/TestUtilsTest.kt") }
+            expect(e.lineNumber) { src.position.get().line }
         }
     }
 })
