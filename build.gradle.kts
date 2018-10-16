c49438ff6df59a04bafbae20cae7b9f74d181350
Martin Vysny <martin@vysny.me>
2018-10-16 10:03:17 +0300

    build.gradle refactoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,61 @@ subprojects {
         plugin("com.jfrog.bintray")
     }
 
+    // creates a reusable function which configures proper deployment to Bintray
+    ext["configureBintray"] = { artifactId: String ->
+
+        val local = Properties()
+        val localProperties: File = rootProject.file("local.properties")
+        if (localProperties.exists()) {
+            localProperties.inputStream().use { local.load(it) }
+        }
+
+        val java: JavaPluginConvention = convention.getPluginByName("java")
+
+        val sourceJar = task("sourceJar", Jar::class) {
+            dependsOn(tasks.findByName("classes"))
+            classifier = "sources"
+            from(java.sourceSets["main"].allSource)
+        }
+
+        publishing {
+            publications {
+                create("mavenJava", MavenPublication::class.java).apply {
+                    groupId = project.group.toString()
+                    this.artifactId = artifactId
+                    version = project.version.toString()
+                    pom.withXml {
+                        val root = asNode()
+                        root.appendNode("description", "Simple Dynamic Testing Framework piggybacking on JUnit5")
+                        root.appendNode("name", artifactId)
+                        root.appendNode("url", "https://github.com/mvysny/dynatest")
+                    }
+                    from(components.findByName("java")!!)
+                    artifact(sourceJar) {
+                        classifier = "sources"
+                    }
+                }
+            }
+        }
+
+        bintray {
+            user = local.getProperty("bintray.user")
+            key = local.getProperty("bintray.key")
+            pkg(closureOf<BintrayExtension.PackageConfig> {
+                repo = "github"
+                name = "com.github.mvysny.dynatest"
+                setLicenses("Apache-2.0")
+                vcsUrl = "https://github.com/mvysny/dynatest"
+                publish = true
+                setPublications("mavenJava")
+                version(closureOf<BintrayExtension.VersionConfig> {
+                    this.name = project.version.toString()
+                    released = Date().toString()
+                })
+            })
+        }
+    }
+
     tasks.withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "1.8"
     }
--- dynatest-engine/build.gradle.kts
+++ dynatest-engine/build.gradle.kts
@@ -12,54 +12,5 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.0")
 }
 
-val org.gradle.api.Project.java2: org.gradle.api.plugins.JavaPluginConvention get() =
-    convention.getPluginByName("java")
-
-val sourceJar = task("sourceJar", Jar::class) {
-    dependsOn(tasks.findByName("classes"))
-    classifier = "sources"
-    from(java2.sourceSets["main"].allSource)
-}
-
-publishing {
-    publications {
-        create("mavenJava", MavenPublication::class.java).apply {
-            groupId = project.group.toString()
-            artifactId = "dynatest-engine"
-            version = project.version.toString()
-            pom.withXml {
-                val root = asNode()
-                root.appendNode("description", "Simple Dynamic Testing Framework piggybacking on JUnit5")
-                root.appendNode("name", "DynaTest")
-                root.appendNode("url", "https://github.com/mvysny/dynatest")
-            }
-            from(components.findByName("java")!!)
-            artifact(sourceJar) {
-                classifier = "sources"
-            }
-        }
-    }
-}
-
-bintray {
-    val local = Properties()
-    val localProperties: File = project.rootProject.file("local.properties")
-    if (localProperties.exists()) {
-        localProperties.inputStream().use { local.load(it) }
-    }
-
-    user = local.getProperty("bintray.user")
-    key = local.getProperty("bintray.key")
-    pkg(closureOf<BintrayExtension.PackageConfig> {
-        repo = "github"
-        name = "com.github.mvysny.dynatest"
-        setLicenses("Apache-2.0")
-        vcsUrl = "https://github.com/mvysny/dynatest"
-        publish = true
-        setPublications("mavenJava")
-        version(closureOf<BintrayExtension.VersionConfig> {
-            this.name = project.version.toString()
-            released = Date().toString()
-        })
-    })
-}
+val configureBintray = ext["configureBintray"] as (artifactId: String) -> Unit
+configureBintray("dynatest-engine")
