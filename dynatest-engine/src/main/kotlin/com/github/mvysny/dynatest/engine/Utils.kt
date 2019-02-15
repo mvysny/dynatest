d2d79c8098e8a4d8681a7e9d25e76e2b36e36b5d
Martin Vysny <martin@vysny.me>
2019-02-15 16:24:09 +0200

    First attempt to fix DynaTest on Java9

--- dynatest-engine/src/main/kotlin/com/github/mvysny/dynatest/engine/Utils.kt
+++ dynatest-engine/src/main/kotlin/com/github/mvysny/dynatest/engine/Utils.kt
@@ -8,8 +8,23 @@ import java.nio.file.FileSystemNotFoundException
 import java.nio.file.Paths
 
 internal val isRunningInsideGradle: Boolean get() {
-    val jars: List<URL> = (Thread.currentThread().contextClassLoader as URLClassLoader).urLs.toList()
-    return (jars.any { it.toString().contains("gradle-worker.jar") })
+    // if this function fails, Gradle will freeze. What the fuck!
+    try {
+        val classLoader = Thread.currentThread().contextClassLoader
+        if (classLoader is URLClassLoader) {
+            // JDK 8 and older
+            val jars: List<URL> = classLoader.urLs.toList()
+            return (jars.any { it.toString().contains("gradle-worker.jar") })
+        }
+        // JDK 9+ uses AppClassLoader
+        val methodGetUrls = classLoader::class.java.getDeclaredMethod("getURLs")
+        val jars = methodGetUrls.invoke(classLoader) as Array<URL>
+        return (jars.any { it.toString().contains("gradle-worker.jar") })
+    } catch (t: Throwable) {
+        // give up, just pretend that we're inside of Gradle
+        t.printStackTrace()
+        return true
+    }
 }
 
 internal fun URI.toFile(): File? {
