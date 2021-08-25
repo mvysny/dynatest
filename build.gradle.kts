56317824d0a94d7001e5aa26297a6aa0dd161b00
Martin Vysny <mavi@vaadin.com>
2021-08-25 12:32:09 +0300

    gradle v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,8 @@ subprojects {
         java {
             withJavadocJar()
             withSourcesJar()
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
         }
 
         tasks.withType<Javadoc> {
